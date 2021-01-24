8ab23fb4a77c48ac11e64df579961a911545e04a
bharatrajput2409 <nutboltcode@gmail.com>
2021-01-24 18:36:14 +0530

    second commit

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('profile',views.profile, name='profile'),
-    path('signin' , views.signin , name='signin'),
-    path('register',views.register,name="register"),
-    path('logout',views.logout,name='logout')
-]
